2a333a0411a08a9c11825f491f7538eccb4e7142
FeminaBrahmbhatt <Feminabrahmbhatt6@gmail.com>
2017-09-02 11:14:52 +0530

    Initial Commit

--- CollectionviewDemo/CollectionViewCell2.h
+++ CollectionviewDemo/CollectionViewCell2.h
@@ -3,7 +3,7 @@
 //  CollectionviewDemo
 //
 //  Created by Femina Rajesh Brahmbhatt on 27/07/17.
-//  Copyright © 2017 ENM Technologies. All rights reserved.
+//  Copyright © 2017 Femina Rajesh Brahmbhatt. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
